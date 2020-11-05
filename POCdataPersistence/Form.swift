1b853b9c1c8de902e14d6ac5a13928661216a43e
BrunoCA-xD <bruno.car.ambrosio@gmail.com>
2020-11-05 13:23:31 -0300

    initial commit 2

---
+++ POCdataPersistence/Form.swift
@@ -0,0 +1,14 @@
+//
+//  Form.swift
+//  POCdataPersistence
+//
+//  Created by Bruno Cardoso Ambrosio on 04/11/20.
+//
+
+import Foundation
+
+struct Form: Codable {
+    var name: String
+    var imc: Float
+    var activitylevel: String
+}
--- POCdataPersistence/ViewController.swift
+++ POCdataPersistence/ViewController.swift
@@ -12,8 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let form = Form(name: "Bruno", imc: 25.45, activitylevel: "Sedentário")
+        do {
+            let data = try JSONEncoder().encode(form)
+            let myJson = try JSONSerialization.jsonObject(with: data, options: [])
+            print("My JSON: ", myJson)
+        } catch {
+            print(error)
+        }
     }
 
+    
 
 }
 
