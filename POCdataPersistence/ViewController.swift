3de227000dee9421947708857a35ad43dad57c0b
BrunoCA-xD <bruno.car.ambrosio@gmail.com>
2020-11-05 13:47:34 -0300

    reading and wrinting a json file

--- POCdataPersistence/ViewController.swift
+++ POCdataPersistence/ViewController.swift
@@ -12,11 +12,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let operation = 1 // 0 - salvar 1 - puxar
+        
         let form = Form(name: "Bruno", imc: 25.45, activitylevel: "Sedentário")
         do {
-            let data = try JSONEncoder().encode(form)
-            let myJson = try JSONSerialization.jsonObject(with: data, options: [])
-            print("My JSON: ", myJson)
+            var url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            url.appendPathComponent("/mandala.json")
+            if operation == 0{
+                let data = try JSONEncoder().encode(form)
+                let myJson = try JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
+                // SALVAR E LER DE ARQUIVO
+                try myJson.write(to: url,
+                                 atomically: true)
+                print("My JSON: \n", myJson)
+            }else {
+                var file = NSDictionary(contentsOf: url)
+                
+                let readJSON = try JSONSerialization.data(withJSONObject: file, options: [])
+                let decoded = try JSONDecoder().decode(Form.self, from: readJSON)
+                print(decoded)
+            }
+            
+//            let readJSON = try JSONSerialization.data(withJSONObject: myJson, options: [])
+//            let decoded = try JSONDecoder().decode(Form.self, from: readJSON)
+//            print(decoded)
         } catch {
             print(error)
         }
