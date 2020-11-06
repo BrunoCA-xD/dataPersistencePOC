3f201b69facfb51fda708ecaa747b888b40d5681
Giovani Coutinho <giocoutinho26@gmail.com>
2020-11-06 16:10:39 -0300

    put the code generic

--- POCdataPersistence/JSONManager.swift
+++ POCdataPersistence/JSONManager.swift
@@ -9,31 +9,47 @@ import Foundation
 import UIKit
 
 class JSONManager{
-    func saveJSON(json:Form,fileName:String){
+    private func getURL(fileName: String) -> URL?{
         do {
             var url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
             url.appendPathComponent(fileName)
-            let data = try JSONEncoder().encode(json)
-            let myJson = try JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
-            // SALVAR ARQUIVO
-            myJson.write(to: url, atomically: true)
+            return url
+        }catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    func saveJSON(json: Form, fileName: String){
+        do {
+            if let url = getURL(fileName: fileName) {
+                let data = try JSONEncoder().encode(json)
+                let myJson = try JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
+                // SALVAR ARQUIVO
+                myJson.write(to: url, atomically: true)
+            }else{
+                print("Failed generating url")
+            }
         } catch {
             print(error)
         }
     }
     
-    func getJSON(fileName:String) -> Form{
+    func getJSON(fileName: String) -> Form?{
         do {
-            var url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            url.appendPathComponent(fileName)
-            let file = NSDictionary(contentsOf: url)
-            // LER ARQUIVO
-            let readJSON = try JSONSerialization.data(withJSONObject: file!, options: [])
-            let decoded = try JSONDecoder().decode(Form.self, from: readJSON)
-            return decoded
+            if let url = getURL(fileName: fileName){
+                let file = NSDictionary(contentsOf: url)
+                // LER ARQUIVO
+                let readJSON = try JSONSerialization.data(withJSONObject: file!, options: [])
+                let decoded = try JSONDecoder().decode(Form.self, from: readJSON)
+                return decoded
+            }else{
+                print("failed generating url")
+                return nil
+            }
         } catch {
             print(error)
-            return Form(name: "", imc: 0.0, activitylevel: "")
+            return nil
         }
     }
 }
