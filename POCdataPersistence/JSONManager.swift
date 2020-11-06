781a80bf4d9ed9a77ae62c2b439bbfae6f0ed62a
Giovani Coutinho <giocoutinho26@gmail.com>
2020-11-06 16:18:03 -0300

    now it is right

--- POCdataPersistence/JSONManager.swift
+++ POCdataPersistence/JSONManager.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class JSONManager{
+class JSONManager<Entity> where Entity:Codable{
     private func getURL(fileName: String) -> URL?{
         do {
             var url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
@@ -20,7 +20,7 @@ class JSONManager{
         }
     }
     
-    func saveJSON(json: Form, fileName: String){
+    func saveJSON(json: Entity, fileName: String){
         do {
             if let url = getURL(fileName: fileName) {
                 let data = try JSONEncoder().encode(json)
@@ -35,16 +35,16 @@ class JSONManager{
         }
     }
     
-    func getJSON(fileName: String) -> Form?{
+    func getJSON(fileName: String) -> Entity?{
         do {
             if let url = getURL(fileName: fileName){
                 let file = NSDictionary(contentsOf: url)
                 // LER ARQUIVO
                 let readJSON = try JSONSerialization.data(withJSONObject: file!, options: [])
-                let decoded = try JSONDecoder().decode(Form.self, from: readJSON)
+                let decoded = try JSONDecoder().decode(Entity.self, from: readJSON)
                 return decoded
             }else{
-                print("failed generating url")
+                print("Failed generating url")
                 return nil
             }
         } catch {
--- POCdataPersistence/ViewController.swift
+++ POCdataPersistence/ViewController.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var manager = JSONManager()
+    var manager = JSONManager<Form>()
     override func viewDidLoad() {
         super.viewDidLoad()
         let form = Form(name: "Bruno", imc: 25.45, activitylevel: "Sedentário")
         manager.saveJSON(json: form,fileName: "/mandala.json")
-        let reading = manager.getJSON(fileName: "/mandala.json")
-        print(reading)
+        if let reading = manager.getJSON(fileName: "/mandala.json"){
+            print(reading)
+        }
     }
 }
 
