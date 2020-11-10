18816ca548df88b1528d41a50c93bae980e336b2
Giovani Coutinho <giocoutinho26@gmail.com>
2020-11-10 16:28:38 -0300

    problems in concurrency: the last save override the file and data can be older

--- POCdataPersistence/ViewController.swift
+++ POCdataPersistence/ViewController.swift
@@ -11,10 +11,29 @@ class ViewController: UIViewController {
     var manager = JSONManager<Form>()
     override func viewDidLoad() {
         super.viewDidLoad()
-        let form = Form(name: "Giovani", imc: 24.16, activitylevel: "Ativo")
-        manager.saveJSON(json: form,fileName: "/mandala.json")
-        if let reading = manager.getJSON(fileName: "/mandala.json"){
-            print(reading)
+//        let form = Form(name: "Giovani", imc: 24.16, activitylevel: "Ativo")
+//        manager.saveJSON(json: form,fileName: "/mandala.json")
+//        if let reading = manager.getJSON(fileName: "/mandala.json"){
+//            print(reading)
+//        }
+        DispatchQueue.main.async { [self] in
+            if var reading = manager.getJSON(fileName: "/mandala.json"){
+                DispatchQueue(label: "t2").async { [self] in
+                    reading.imc = 18.0
+                    manager.saveJSON(json: reading,fileName: "/mandala.json")
+                }
+                DispatchQueue(label: "t4").async { [self] in
+                    reading.imc = 20.0
+                    manager.saveJSON(json: reading,fileName: "/mandala.json")
+                }
+                
+            }
+        }
+        
+        DispatchQueue.main.async { [self] in
+            if let reading = manager.getJSON(fileName: "/mandala.json"){
+                print(reading)
+            }
         }
     }
 }
