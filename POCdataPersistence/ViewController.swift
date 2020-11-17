2262d9d79eef9d188e4f7711c441c6160f88276a
BrunoCA-xD <bruno.car.ambrosio@gmail.com>
2020-11-17 15:04:50 -0300

    another test and another fail

--- POCdataPersistence/ViewController.swift
+++ POCdataPersistence/ViewController.swift
@@ -8,24 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let operationQueue = OperationQueue()
     var manager = JSONManager<Form>()
     override func viewDidLoad() {
         super.viewDidLoad()
+        operationQueue.maxConcurrentOperationCount = 2
         
     }
     @IBAction func queries(_ sender: Any) {
+        print("Aqui começa")
         manager.getJSON(fileName: "/mandala.json") {mandala, _ in
+            print("lendo")
             if var mandala = mandala {
-                mandala.imc = 234
-                self.manager.saveJSON(json: mandala, fileName: "/mandala.json")
-                mandala.imc = 120
-                self.manager.saveJSON(json: mandala,fileName: "/mandala.json")
+                print("save")
+                self.operationQueue.addOperation {
+                    mandala.imc = 234
+                    self.manager.saveJSON(json: mandala, fileName: "/mandala.json")
+                    print("escrevendo")
+                }
+                print("save1")
+                self.operationQueue.addOperation  {
+                    mandala.imc = 120
+                    self.manager.saveJSON(json: mandala,fileName: "/mandala.json")
+                    print("escrevendo1")
+                }
             }
         }
-        manager.getJSON(fileName: "/mandala.json"){
-            mandala, error in
-            print(mandala)
+        
+        self.operationQueue.addOperation {
+            print("test")
+            self.manager.getJSON(fileName: "/mandala.json"){
+                mandala, error in
+                print("lendo1")
+                print(mandala)
+            }
         }
+        
     }
     @IBAction func save(_ sender: Any) {
         let form = Form(name: "Giovani", imc: 24.16, activitylevel: "Ativo")
